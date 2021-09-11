422d943c71e536a993e425004792dacadcdbd280
Wayne Eaker <wayne@zengenuity.com>
2021-09-11 19:39:54 -0400

    Non-functional customization of the unpublish_escaped Drush command.

--- web/modules/custom/zoopal_geolocation/src/Commands/ZoopalGeolocationCommands.php
+++ web/modules/custom/zoopal_geolocation/src/Commands/ZoopalGeolocationCommands.php
@@ -3,6 +3,8 @@
 namespace Drupal\zoopal_geolocation\Commands;
 
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\zoopal_geolocation\GeolocationManagerInterface;
 use Drush\Commands\DrushCommands;
 
 /**
@@ -19,52 +21,37 @@ use Drush\Commands\DrushCommands;
 class ZoopalGeolocationCommands extends DrushCommands {
 
   /**
-   * Command description here.
-   *
-   * @param $arg1
-   *   Argument description.
-   * @param array $options
-   *   An associative array of options whose values come from cli, aliases, config, etc.
-   * @option option-name
-   *   Description
-   * @usage zoopal_geolocation-commandName foo
-   *   Usage description
-   *
-   * @command zoopal_geolocation:commandName
-   * @aliases foo
+   * @var EntityTypeManagerInterface
    */
-  public function commandName($arg1, $options = ['option-name' => 'default']) {
-    $this->logger()->success(dt('Achievement unlocked.'));
+  protected $entityTypeManager;
+
+  /**
+   * @var GeolocationManagerInterface
+   */
+  protected $geolocationManager;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, GeolocationManagerInterface $geolocationManager)
+  {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->geolocationManager = $geolocationManager;
   }
 
   /**
-   * An example of the table output format.
+   * Unpublishes creatures that have escaped
    *
-   * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
-   *
-   * @field-labels
-   *   group: Group
-   *   token: Token
-   *   name: Name
-   * @default-fields group,token,name
-   *
-   * @command zoopal_geolocation:token
-   * @aliases token
-   *
-   * @filter-default-field name
-   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
+   * @command zoopal:unpublish_escaped
+   * @aliases yikes
    */
-  public function token($options = ['format' => 'table']) {
-    $all = \Drupal::token()->getInfo();
-    foreach ($all['tokens'] as $group => $tokens) {
-      foreach ($tokens as $key => $token) {
-        $rows[] = [
-          'group' => $group,
-          'token' => $key,
-          'name' => $token['name'],
-        ];
-      }
+  public function unpublishEscapedCreatures() {
+    $creature_storage = $this->entityTypeManager->getStorage('zoopal_creature');
+    $results = $creature_storage->getQuery()
+      ->condition('status', TRUE)
+      ->condition('zoopal_location_handler', 'zoopal_geolocation')
+      ->execute();
+    foreach ($results as $creature_id) {
+      $creature = $creature_storage->load($creature_id);
+      $this->writeln($creature->label() . ': ' . $creature->get('zoopal_geolocation_id')->value);
     }
-    return new RowsOfFields($rows);
   }
+
 }
