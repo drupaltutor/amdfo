5e4516918b87b03c4b899f88d155710dfe149e01
Wayne Eaker <wayne@zengenuity.com>
2021-09-11 14:32:15 -0400

    Generated and customized ZooPal species config entity

---
+++ web/modules/custom/zoopal_species/src/Entity/ZoopalSpecies.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\zoopal_species\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\zoopal_species\ZoopalSpeciesInterface;
+
+/**
+ * Defines the species entity type.
+ *
+ * @ConfigEntityType(
+ *   id = "zoopal_species",
+ *   label = @Translation("Species"),
+ *   label_collection = @Translation("Species"),
+ *   label_singular = @Translation("species"),
+ *   label_plural = @Translation("species"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count species",
+ *     plural = "@count specieses",
+ *   ),
+ *   handlers = {
+ *     "list_builder" = "Drupal\zoopal_species\ZoopalSpeciesListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\zoopal_species\Form\ZoopalSpeciesForm",
+ *       "edit" = "Drupal\zoopal_species\Form\ZoopalSpeciesForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm"
+ *     }
+ *   },
+ *   config_prefix = "zoopal_species",
+ *   admin_permission = "administer zoopal_species",
+ *   links = {
+ *     "collection" = "/admin/zoopal/config/species",
+ *     "add-form" = "/admin/zoopal/config/species/add",
+ *     "edit-form" = "/admin/zoopal/config/species/{zoopal_species}",
+ *     "delete-form" = "/admin/zoopal/config/species/{zoopal_species}/delete"
+ *   },
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "scientific_name",
+ *     "uuid" = "uuid"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "scientific_name",
+ *     "common_names",
+ *     "description"
+ *   }
+ * )
+ */
+class ZoopalSpecies extends ConfigEntityBase implements ZoopalSpeciesInterface {
+
+  /**
+   * The species ID.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The species scientific name.
+   *
+   * @var string
+   */
+  protected $scientific_name;
+
+  /**
+   * The species common names
+   *
+   * @var array
+   */
+  protected $common_names;
+
+  /**
+   * The species status.
+   *
+   * @var bool
+   */
+  protected $status;
+
+  /**
+   * The zoopal_species description.
+   *
+   * @var string
+   */
+  protected $description;
+
+}
---
+++ web/modules/custom/zoopal_species/src/Form/ZoopalSpeciesForm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\zoopal_species\Form;
+
+use Drupal\Core\Entity\EntityForm;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Species form.
+ *
+ * @property \Drupal\zoopal_species\ZoopalSpeciesInterface $entity
+ */
+class ZoopalSpeciesForm extends EntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+
+    $form = parent::form($form, $form_state);
+
+    $form['scientific_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Scientfic Name'),
+      '#maxlength' => 255,
+      '#default_value' => $this->entity->label(),
+      '#description' => $this->t('Scientific name for the species.'),
+      '#required' => TRUE,
+    ];
+
+    $form['id'] = [
+      '#type' => 'machine_name',
+      '#default_value' => $this->entity->id(),
+      '#machine_name' => [
+        'exists' => '\Drupal\zoopal_species\Entity\ZoopalSpecies::load',
+        'source' => ['scientific_name'],
+      ],
+      '#disabled' => !$this->entity->isNew(),
+    ];
+
+    $form['status'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enabled'),
+      '#default_value' => $this->entity->status(),
+    ];
+
+    $common_names = $this->entity->get('common_names');
+    $form['common_names'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Common Names'),
+      '#default_value' => ($common_names !== NULL) ? implode(', ', $common_names) : '',
+      '#description' => $this->t('Common names of the species.'),
+    ];
+
+    $form['description'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Description'),
+      '#default_value' => $this->entity->get('description'),
+      '#description' => $this->t('Description of the species.'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $common_names = explode(',', $form_state->getValue('common_names'));
+    $common_names = array_map('trim', $common_names);
+    $this->entity->set('common_names', $common_names);
+
+    $result = parent::save($form, $form_state);
+    $message_args = ['%label' => $this->entity->label()];
+    $message = $result == SAVED_NEW
+      ? $this->t('Created new species %label.', $message_args)
+      : $this->t('Updated species %label.', $message_args);
+    $this->messenger()->addStatus($message);
+    $form_state->setRedirectUrl($this->entity->toUrl('collection'));
+    return $result;
+  }
+
+}
