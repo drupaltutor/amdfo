f397d1a2bcd1b808ec388ba7c1b44103819d19a4
Wayne Eaker <wayne@zengenuity.com>
2021-09-11 18:37:01 -0400

    Added Habitat location handler plugin

---
+++ web/modules/custom/zoopal_habitat/src/Plugin/ZoopalLocationHandler/HabitatLocationHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\zoopal_habitat\Plugin\ZoopalLocationHandler;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\zoopal_location\ZoopalLocationHandlerPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the zoopal_location.
+ *
+ * @ZoopalLocationHandler(
+ *   id = "zoopal_habitat",
+ *   label = @Translation("Habitat"),
+ * )
+ */
+class HabitatLocationHandler extends ZoopalLocationHandlerPluginBase implements ContainerFactoryPluginInterface
+{
+
+  /**
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  protected function setEntityTypeManager(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Creates an instance of the plugin.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container to pull out services used in the plugin.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   *   Returns an instance of this plugin.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    $instance = new static($configuration, $plugin_id, $plugin_id);
+    $instance->setEntityTypeManager($container->get('entity_type.manager'));
+    return $instance;
+  }
+
+
+  public function render(FieldItemInterface $item) {
+    $habitat = $item->getEntity()->get('zoopal_habitat')->entity;
+    if (!empty($habitat)) {
+      $view = $this->entityTypeManager->getViewBuilder('zoopal_habitat')->view($habitat);
+      return $view;
+    }
+    return [];
+  }
+}
